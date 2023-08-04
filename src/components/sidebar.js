71a5e514c5e5665f7a6137a3d85e5bbcfbc862cb
yidid <yididiya07@gmail.com>
2023-08-04 17:22:58 +0300

    initial commit

---
+++ src/components/sidebar.js
@@ -0,0 +1,46 @@
+
+<div className="min-h-screen flex flex-row bg-gray-100">
+  <div className="flex flex-col w-56 bg-white overflow-hidden">
+   
+    <ul className="flex flex-col py-4">
+      <li>
+        <a href="" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-home"></i></span>
+          <span className="text-sm font-medium">Dashboard</span>
+        </a>
+      </li>
+      <li>
+        <a href="" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-admin"></i></span>
+          <span className="text-sm font-medium">Admin</span>
+        </a>
+      </li>
+      <li>
+        <a href="" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-drink"></i></span>
+          <span className="text-sm font-medium">Rescures</span>
+        </a>
+      </li>
+   
+      <li>
+        <a href="" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-user"></i></span>
+          <span className="text-sm font-medium">General</span>
+        </a>
+      </li>
+      <li>
+        <a href="" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-bell"></i></span>
+          <span className="text-sm font-medium">Notifications</span>
+          <span className="ml-auto mr-6 text-sm bg-red-100 rounded-full px-3 py-px text-red-500">5</span>
+        </a>
+      </li>
+      <li>
+        <a href="" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-log-out"></i></span>
+          <span className="text-sm font-medium">Logout</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+</div>
---
+++ src/pages/Community_chat.js
@@ -0,0 +1,151 @@
+import React from "react";
+
+const Community= ()=>{
+    return(
+        <div>
+        <nav className="sticky top-0 z-50 flex w-full flex-wrap text-center bg-[#FBFBFB] py-2 text-purple-500 shadow-lg focus:text-neutral-700 dark:bg-slate-50 lg:py-4">
+        <div className="flex w-full flex-wrap  px-3">
+      
+        <img src='./images/ndm.png' className="w-14 rounded-sm "alt="Avatar" /> 
+        <div class="pl-96 ...">
+        <h1 className=" text-4xl font-bold font-serif text-center">Community chat</h1>
+        </div>
+        <div class="pl-96 ...">
+        <img src="https://tecdn.b-cdn.net/img/new/avatars/2.webp" className="w-12 rounded-full "alt="Avatar" /> 
+        </div>
+  </div>
+</nav>
+
+<link rel="stylesheet" href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" />
+
+<div className="min-h-screen flex flex-row bg-gray-100">
+  <div className="flex flex-col w-56 bg-white overflow-hidden">
+   
+    <ul className="flex flex-col py-4">
+      <li>
+        <a href="" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-home"></i></span>
+          <span className="text-sm font-medium">Dashboard</span>
+        </a>
+      </li>
+      <li>
+        <a href="" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-cloud-lightning"></i></span>
+          <span className="text-sm font-medium">Disasters</span>
+        </a>
+      </li>
+      <li>
+        <a href="" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-chat"></i></span>
+          <span className="text-sm font-medium">Community</span>
+        </a>
+      </li>
+   
+      <li>
+        <a href="" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-money"></i></span>
+          <span className="text-sm font-medium">Donation</span>
+        </a>
+      </li>
+      <li>
+        <a href="" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-first-aid"></i></span>
+          <span className="text-sm font-medium">Fast Responders</span>
+        </a>
+      </li>
+      <li>
+        <a href="" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-bell"></i></span>
+          <span className="text-sm font-medium">Notifications</span>
+          <span className="ml-auto mr-6 text-sm bg-red-100 rounded-full px-3 py-px text-red-500">5</span>
+        </a>
+      </li>
+      <li>
+        <a href="" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-log-out"></i></span>
+          <span className="text-sm font-medium">Logout</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+  <div class="w-fit px-5 flex flex-col justify-between">
+        <div class="flex flex-col mt-3">
+          <div class="flex justify-end mb-4">
+            <div
+              class="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
+              Welcome to group everyone !
+            </div>
+            <img
+              src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
+              class="object-cover h-8 w-8 rounded-full"
+              alt=""
+            />
+          </div>
+          <div class="flex justify-start mb-4">
+            <img
+              src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
+              class="object-cover h-8 w-8 rounded-full"
+              alt=""
+            />
+            <div
+              class="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white"
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+              at praesentium, aut ullam delectus odio error sit rem. Architecto
+              nulla doloribus laborum illo rem enim dolor odio saepe,
+              consequatur quas?
+            </div>
+          </div>
+          <div class="flex justify-end mb-4">
+            <div>
+              <div
+                class="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white"
+              >
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Magnam, repudiandae.
+              </div>
+
+              <div
+                class="mt-4 mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white"
+              >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Debitis, reiciendis!
+              </div>
+            </div>
+            <img
+              src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
+              class="object-cover h-8 w-8 rounded-full"
+              alt=""
+            />
+          </div>
+          <div class="flex justify-start mb-4">
+            <img
+              src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
+              class="object-cover h-8 w-8 rounded-full"
+              alt=""
+            />
+            <div
+              class="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white"
+            >
+              happy holiday guys!
+            </div>
+
+            
+          </div>
+          
+        <div class="pt-36">
+          <input
+            class=" sticky  bottom-0 z-50 w-full bg-gray-300 py-3 px-3 rounded-xl"
+            type="text"
+            placeholder="type your message here..."
+          />
+        </div>
+
+        
+        </div>
+      </div>
+    
+</div>
+      </div>
+    )}
+export default Community
