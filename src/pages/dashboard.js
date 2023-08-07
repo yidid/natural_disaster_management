a738df6640fe594066ba575bc3f3be51ec4e916a
yidid <yididiya07@gmail.com>
2023-08-07 12:47:09 +0300

    adding new pages

---
+++ src/pages/dashboard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import CommunityAdmin from "../components/sidebar";
+
+
+const Dashboard = ()=>{
+    return(
+        <div>
+              <CommunityAdmin name="Dashboard"/>
+             <div class="block rounded-lg  z-1 bg-gray-50 ml-64 mb-96 p-36 m-11 dark:bg-grey-100">
+               </div>
+             
+             </div>
+         
+        
+        
+    )
+}
+
+export default Dashboard
---
+++ src/pages/responders.js
@@ -0,0 +1,24 @@
+import React from "react";
+import CommunityAdmin from "../components/sidebar";
+
+
+const FastResponders = ()=>{
+    return(
+        <div>
+             
+            
+             <CommunityAdmin name="Fast responders"/>
+           
+             <div class="block rounded-lg  z-1 bg-gray-50 ml-64 mb-96 p-36 m-11 dark:bg-grey-100">
+                
+ 
+               </div>
+             
+             </div>
+         
+        
+        
+    )
+}
+
+export default FastResponders
