1c32ec807f9771f476020bbde326a38e087711c4
yidid <yididiya07@gmail.com>
2023-08-07 12:50:55 +0300

    adding some modification and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Newpage from './pages/disasters';
 import Community from './pages/Community_chat';
+import Dashboard from './pages/dashboard';
+import Donation from './pages/donation';
+import FastResponders from './pages/responders';
 
 function App() {
   return (
-    
-      <Community/>
+    <Router>
+    <Routes>
+    <Route path='/community' element={<Community/>}></Route>
+    <Route path='/disaster' element={<Newpage />}></Route>
+    <Route path='/' element={<Dashboard />}></Route>
+    <Route path='/donation' element={<Donation />}></Route>
+    <Route path='/responders' element={<FastResponders />}></Route>
+   </Routes>
+   </Router>  
+     
     
   );
 }
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,46 +1,80 @@
+import React from "react";
 
-<div className="min-h-screen flex flex-row bg-gray-100">
-  <div className="flex flex-col w-56 bg-white overflow-hidden">
-   
-    <ul className="flex flex-col py-4">
-      <li>
-        <a href="" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-home"></i></span>
-          <span className="text-sm font-medium">Dashboard</span>
-        </a>
-      </li>
-      <li>
-        <a href="" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-admin"></i></span>
-          <span className="text-sm font-medium">Admin</span>
-        </a>
-      </li>
-      <li>
-        <a href="" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-drink"></i></span>
-          <span className="text-sm font-medium">Rescures</span>
-        </a>
-      </li>
-   
-      <li>
-        <a href="" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-user"></i></span>
-          <span className="text-sm font-medium">General</span>
-        </a>
-      </li>
-      <li>
-        <a href="" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-bell"></i></span>
-          <span className="text-sm font-medium">Notifications</span>
-          <span className="ml-auto mr-6 text-sm bg-red-100 rounded-full px-3 py-px text-red-500">5</span>
-        </a>
-      </li>
-      <li>
-        <a href="" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-          <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-log-out"></i></span>
-          <span className="text-sm font-medium">Logout</span>
-        </a>
-      </li>
-    </ul>
-  </div>
+const CommunityAdmin= (props)=>{
+  return(
+    <>
+    
+    <nav className="sticky top-0 z-50 flex w-full flex-wrap text-center bg-[#FBFBFB] py-2 text-purple-500 shadow-lg focus:text-neutral-700 dark:bg-slate-50 lg:py-4">
+    <div className="flex w-full flex-wrap  px-3">
+  
+    <img src='./images/ndm.png' className="w-14 rounded-sm "alt="Avatar" /> 
+    <div class="pl-96 ...">
+    <h1 className=" text-4xl font-bold font-serif text-center">{props.name}</h1>
+    </div>
+    <div class="pl-96 ...">
+    <img src="https://tecdn.b-cdn.net/img/new/avatars/2.webp" className="w-12 rounded-full "alt="Avatar" /> 
+    </div>
 </div>
+</nav>
+
+<link rel="stylesheet" href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" />
+
+<div className="min-h-screen flex flex-row bg-gray-100 absolute z-0">
+<div className="flex flex-col w-56 bg-gray-50 overflow-hidden">
+
+<ul className="flex flex-col py-4">
+  <li>
+    <a href="/" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+      <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-home"></i></span>
+      <span className="text-sm font-medium">Dashboard</span>
+    </a>
+  </li>
+  <li>
+    <a href="/disaster" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+      <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-cloud-lightning"></i></span>
+      <span className="text-sm font-medium">Disasters</span>
+    </a>
+  </li>
+  <li>
+    <a href="/community" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+      <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-chat"></i></span>
+      <span className="text-sm font-medium">Community</span>
+    </a>
+  </li>
+
+  <li>
+    <a href="/donation" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+      <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-money"></i></span>
+      <span className="text-sm font-medium">Donation</span>
+    </a>
+  </li>
+  <li>
+    <a href="/responders" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+      <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-first-aid"></i></span>
+      <span className="text-sm font-medium">Fast Responders</span>
+    </a>
+  </li>
+  <li>
+    <a href="/" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+      <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-bell"></i></span>
+      <span className="text-sm font-medium">Notifications</span>
+      <span className="ml-auto mr-6 text-sm bg-red-100 rounded-full px-3 py-px text-red-500">5</span>
+    </a>
+  </li>
+  <li>
+    <a href="/" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+      <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-log-out"></i></span>
+      <span className="text-sm font-medium">Logout</span>
+    </a>
+  </li>
+</ul>
+</div>
+
+
+</div>
+  </>
+
+  )
+}
+
+export default CommunityAdmin;
