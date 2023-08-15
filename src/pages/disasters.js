7a0bca3ffeec38d9b59434bf9ea57d5d3d07e216
yididiya <yididiya07@gmail.com>
2023-08-15 22:47:24 +0300

    adding disaster page

--- src/pages/disasters.js
+++ src/pages/disasters.js
@@ -1,5 +1,6 @@
 import React from "react";
 import CommunityAdmin from "../components/sidebar";
+import image from "../images/nd.jpg"
 
 
 const   Newpage = ()=>{
@@ -8,7 +9,23 @@ const   Newpage = ()=>{
 
              <CommunityAdmin name="Natural disasters"/>
            
-             <div class="block rounded-lg  z-1 bg-gray-50 ml-64 mb-96 p-36 m-11 dark:bg-grey-100">
+             <div class="block rounded-lg  z-1 bg-gray-50 ml-64 mb-96 p-24 m-11 dark:bg-grey-100">
+             <div class="max-w-lg rounded overflow-hidden shadow-lg">
+  <img class="w-full" src={image} alt="Sunset in the mountains"></img>
+  <div class="px-6 py-4">
+    <div class="font-extrabold text-xl mb-2 font-serif">The Coldest Sunset</div>
+    <p class="text-gray-700 text-base font-serif">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+      
+    </p>
+  </div>
+  <div class="px-6 pt-4 pb-2">
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+  </div>
+</div>
               
  
               </div>
