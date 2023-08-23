4eb327625367b173095a5c9cc0891a5604d9815b
yidid <yididiya07@gmail.com>
2023-08-23 09:53:32 +0300

    adding new pages

---
+++ src/pages/login.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+
+
+
+
+const Login= ()=>{
+ 
+
+    return(
+
+<div className="w-full min-h-screen bg-gray-50 flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+  <div className="w-full sm:max-w-md p-5 mx-auto">
+    <h2 className="mb-12 text-center text-5xl font-extrabold">Welcome.</h2>
+    <form >
+      <div className="mb-4">
+        <label className="block mb-1" for="email">Email-Address</label>
+        <input id="email" type="text" name="email"   
+       
+          className="py-2 px-3 border border-gray-300 focus:border-purple-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full" />
+      </div>
+      <div className="mb-4">
+        <label className="block mb-1" for="password">Password</label>
+        <input id="password" type="password" name="password" 
+        
+          className="py-2 px-3 border border-gray-300 focus:border-red-300 focus:outline-none focus:ring focus:ring-red-200 focus:ring-opacity-50 rounded-md shadow-sm disabled:bg-gray-100 mt-1 block w-full" />
+      </div>
+      <div className="mt-6 flex items-center justify-between">
+        <div className="flex items-center">
+          <input id="remember_me" type="checkbox" className="border border-gray-300 text-red-600 shadow-sm focus:border-red-300 focus:ring focus:ring-red-200 focus:ring-opacity-50" />
+          <label for="remember_me" className="ml-2 block text-sm leading-5 text-gray-900"> Remember me </label>
+        </div>
+        <a href="#" className="text-sm"> Forgot your password? </a>
+      </div>
+      <div className="mt-6">
+        <button className="w-full inline-flex items-center justify-center px-4 py-2 bg-purple-600 border border-transparent rounded-md font-semibold capitalize text-white hover:bg-purple-700 active:bg-red-700 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 disabled:opacity-25 transition"
+        type='submit '>Sign In</button>
+      </div>
+      <div className="mt-6 text-center">
+        <a href="/signup" className="underline">Sign up for an account</a>
+      </div>
+      <div className="mt-6 text-center">
+        <a href="/admin" className="underline">Are you admin?</a>
+      </div>
+    </form>
+  </div>
+</div>
+
+    )
+}
+
+export default Login
+
---
+++ src/pages/prepare.js
@@ -0,0 +1,121 @@
+import React from "react";
+import NavBar from "../components/navbar";
+
+const Prepare =()=>{
+
+    return(
+       
+            <>
+            <NavBar/>
+
+            <div class="block rounded-lg z-1 bg-gray-50 ml-20 mb-96 p-12 m-11 dark:bg-grey-100 justify-center content-center ">
+         
+         
+
+	<div
+		class="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-gray-800 mb-7">
+		<div class="w-full md:w-1/3 bg-gray-800 grid place-items-center">
+			<img src="https://images.pexels.com/photos/4381392/pexels-photo-4381392.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="tailwind logo" class="rounded-xl" />
+    </div>
+			<div class="w-full md:w-2/3 bg-gray-800 flex flex-col space-y-2 p-3">
+				<div class="flex justify-between item-center">
+					<p class="text-gray-500 font-medium hidden md:block">#1</p>
+					<div class="flex items-center">
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-yellow-500" viewBox="0 0 20 20"
+							fill="currentColor">
+							<path
+								d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+						</svg>
+						
+					</div>
+				</div>
+				<h3 class="font-black text-white md:text-3xl text-xl">Stay informed</h3>
+				<p class="md:text-lg text-gray-400 text-base">It’s important to know what natural disasters could potentially affect your area and how to get emergency alerts that will notify you of anything happening nearby. You can listen on the radio or watch your television, social media channels, or sign up to have notifications sent directly to your smartphone. Depending on where you live, certain severe weather conditions could be more likely to occur (and some could never occur at all).</p>
+				
+			</div>
+		</div>
+
+        <div
+		class="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-gray-800 mb-7">
+		<div class="w-full md:w-1/3 bg-gray-800 grid place-items-center">
+			<img src="https://images.pexels.com/photos/4381392/pexels-photo-4381392.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="tailwind logo" class="rounded-xl" />
+    </div>
+			<div class="w-full md:w-2/3 bg-gray-800 flex flex-col space-y-2 p-3">
+				<div class="flex justify-between item-center">
+					<p class="text-gray-500 font-medium hidden md:block">#1</p>
+					<div class="flex items-center">
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-yellow-500" viewBox="0 0 20 20"
+							fill="currentColor">
+							<path
+								d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+						</svg>
+						
+					</div>
+				</div>
+				<h3 class="font-black text-white md:text-3xl text-xl">Keep Others in Mind</h3>
+				<p class="md:text-lg text-gray-400 text-base"> 
+
+It’s difficult to think ahead especially if you haven’t experienced a natural disaster before, but there are many things you’ll want that you wouldn’t necessarily remember to pack or store. Be sure to have a plan for everyone in your family (including pets). Think about medication, special medical equipment, diapers, and pet food. Make a natural disaster preparedness checklist so that you know exactly what you’ll need for you and for everyone in your life during a natural disaster.</p>
+				
+			</div>		</div>
+
+        <div
+		class="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-gray-800 mb-7">
+		<div class="w-full md:w-1/3 bg-gray-800 grid place-items-center">
+			<img src="https://images.pexels.com/photos/4381392/pexels-photo-4381392.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="tailwind logo" class="rounded-xl" />
+    </div>
+			<div class="w-full md:w-2/3 bg-gray-800 flex flex-col space-y-2 p-3">
+				<div class="flex justify-between item-center">
+					<p class="text-gray-500 font-medium hidden md:block">#1</p>
+					<div class="flex items-center">
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-yellow-500" viewBox="0 0 20 20"
+							fill="currentColor">
+							<path
+								d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+						</svg>
+						
+					</div>
+				</div>
+				<h3 class="font-black text-white md:text-3xl text-xl"> Learn Evacuation Routes and Shelter Locations</h3>
+				<p class="md:text-lg text-gray-400 text-base">Learn your area’s evacuation routes and shelter locations as soon as possible. Knowing these things ahead of time will help with the stress and panic that comes with a sudden crisis situation. Set an escape route from your home to the nearest safe space with your family. Drawing a map and hanging it up somewhere will help younger children remember what to do and where to go. If you’re caught away from home, know how to react from different places, like your workplace or car..</p>
+				
+			</div>
+		</div>
+
+        <div
+		class="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-gray-800 mb-7">
+		<div class="w-full md:w-1/3 bg-gray-800 grid place-items-center">
+			<img src="https://images.pexels.com/photos/4381392/pexels-photo-4381392.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="tailwind logo" class="rounded-xl" />
+    </div>
+			<div class="w-full md:w-2/3 bg-gray-800 flex flex-col space-y-2 p-3">
+				<div class="flex justify-between item-center">
+					<p class="text-gray-500 font-medium hidden md:block">#1</p>
+					<div class="flex items-center">
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-yellow-500" viewBox="0 0 20 20"
+							fill="currentColor">
+							<path
+								d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+						</svg>
+						
+					</div>
+				</div>
+				<h3 class="font-black text-white md:text-3xl text-xl">Stay Connected</h3>
+				<p class="md:text-lg text-gray-400 text-base">Know how you’ll reconnect with people who matter if a disaster were to strike. Your cell service could be affected, which would make it harder to reach family and friends. Make sure you know how to connect with your children if they’re at school, daycare, or any after-school activities. You want to be able to communicate with your loved ones, so always have emergency contact numbers at the ready and prepare different ways to reach out, like a landline phone at home..</p>
+				
+			</div>
+		</div>
+
+
+    
+	</div>
+       
+            
+            </>
+    
+        )
+        
+    
+
+}
+
+export default Prepare 
