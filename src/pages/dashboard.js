dd63cd1f4e830bfce163ad4bbb113a0193ce4e4e
burtemaryam <birtukansol5@gmail.com>
2023-08-31 09:27:19 +0300

    responsive admin dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,42 +1,45 @@
 import React from "react";
-import CommunityAdmin from "../components/sidebar";
+import AdminSidebar from "../components/adminSidebar";
 
+const AdminDashboardNew = () => {
+  const [isMinimized, setIsMinimized] = React.useState(false);
 
-const Dashboard = ()=>{
-    return(
-        <>
-              <CommunityAdmin name="Dashboard"/>
-             <div class="block rounded-lg z-1 bg-gray-50 ml-64 mb-96 p-28 m-11 dark:bg-grey-100">
-           
-             <div class="m grid grid-cols-2 gap-4 ">
-            <div class="box-border h-60 w-78  border-2 bg-cyan-100"> 
-            <h1 className="text-center text-7xl  font-bold font-serif text-gray-700  pt-16">1000</h1> 
-            <h1 className="text-center text-4xl  font-bold font-serif text-gray-700  pt-7">Customers</h1> 
-            </div>
-            <div class="box-border h-60 w-78  border-2 bg-red-200"> 
-            <h1 className="text-center text-7xl  font-bold font-serif text-gray-700   pt-16">1000</h1> 
-            <h1 className="text-center text-4xl  font-bold font-serif text-gray-700  pt-7">Customers</h1> 
-            </div>
-            <div class="box-border h-60 w-78  border-2 bg-lime-100"> 
-            <h1 className="text-center text-7xl  font-bold font-serif text-gray-700   pt-16">1000</h1> 
-            <h1 className="text-center text-4xl  font-bold font-serif text-gray-700  pt-7">Customers</h1> 
-            </div>
-            <div class="box-border h-60 w-78  border-2 bg-slate-300"> 
-            <h1 className="text-center text-7xl  font-bold font-serif text-gray-700   pt-16">1000</h1> 
-            <h1 className="text-center text-4xl  font-bold font-serif text-gray-700  pt-7">Customers</h1> 
-            </div>
-            
- 
-</div>
-</div>
+  const handleResize = () => {
+    if (window.innerWidth < 768) {
+      setIsMinimized(true);
+    } else {
+      setIsMinimized(false);
+    }
+  };
 
-               </>
-             
-             
-         
-        
-        
-    )
-}
+  React.useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
-export default Dashboard
+  return (
+    <>
+      <AdminSidebar name="Dashboard" />
+     
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-10 lg:ml-60 sm:ml-96">
+          <div className={`bg-red-300  text-center w-80 h-20  pt-7 ${isMinimized ? "icon-only" : ""}`}>
+            <h5>{isMinimized ? "1" : "Disaster 100"}</h5>
+          </div>
+          <div className={`bg-blue-300   text-center w-80 h-20 pt-7 ${isMinimized ? "icon-only" : ""}`}>
+            <h5>{isMinimized ? "2" : "Responders 12345"}</h5>
+          </div>
+          <div className={`bg-green-300  text-center w-80 h-20 pt-7 ${isMinimized ? "icon-only" : ""}`}>
+            <h5>{isMinimized ? "3" : "Customer 78453"}</h5>
+          </div>
+          <div className={`bg-orange-300  text-center w-80 h-20 pt-7  ${isMinimized ? "icon-only" : ""}`}>
+            <h5>{isMinimized ? "4" : "Donation 100348659"}</h5>
+          </div>
+        </div>
+    
+    </>
+  );
+};
+
+export default AdminDashboardNew;
